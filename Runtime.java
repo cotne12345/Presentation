e56bb048bbda6c6123cd89307cc4aec663550875
cotne12345 <80108173+cotne12345@users.noreply.github.com>
2021-05-17 18:58:08 +0400

    Add files via upload

---
+++ Runtime.java
@@ -0,0 +1,17 @@
+public class MyExceptionTest {
+    public void testRuntimeException () {
+        throw new MyException();
+    }
+    public static void main(String[] args) {
+        try {
+            new MyExceptionTest().testRuntimeException();
+        } catch(Exception e) {
+            System.out.println(e.getClass().getName());
+        }
+    }
+}
+class MyException extends RuntimeException {
+    public MyException() {
+        super();
+    }
+}
